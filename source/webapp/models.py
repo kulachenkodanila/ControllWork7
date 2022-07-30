15664db1a618435cfd1e2bbaa87584389c49aaa2
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-30 12:18:55 +0300

    Создал модель опроса и сделал миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.urls import reverse
+
+
+class Poll(models.Model):
+    question = models.TextField(max_length=500, verbose_name="Вопрос")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.id}. {self.question}: {self.created_at}"
+
+    class Meta:
+        db_table = "polls"
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
+
+
+# class Choice(models.Model):
+#     text = question = models.TextField(max_length=100, verbose_name="Вариант")
