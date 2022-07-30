d9fb336b8209d3d90101420b87b02f0e1e386083
Kulachenko_Danila <kulachenkodanila@gmail.com>
2022-07-30 12:55:44 +0300

    Зарегистрировал модели в admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'created_at']
+    list_display_links = ['question']
+    list_filter = ['question']
+    search_fields = ['question']
+    fields = ['question', 'created_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
